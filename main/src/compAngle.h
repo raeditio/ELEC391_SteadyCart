264dbe5ec26a40be1ac29e347f50afea4c0282dd
AmritS76 <amrit701@hotmail.com>
2025-03-29 17:31:23 -0700

    Header Definition Corrected

--- main/src/compAngle.h
+++ main/src/compAngle.h
@@ -1,5 +1,5 @@
-#ifndef ACCEL_ANGLE_H
-#define ACCEL_ANGLE_H
+#ifndef COMP_ANGLE_H
+#define COMP_ANGLE_H
 
 void initIMU();         // Initializes the IMU
 float getCompAngle();  // Returns the complementary tilt angle
